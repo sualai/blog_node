fb58ec5f5a2e7fe10670772913702b79c15e5873
sualai <1318768513@qq.com>
2022-07-11 07:50:32 +0800

    查询回复列表

--- controller/replay.js
+++ controller/replay.js
@@ -14,5 +14,14 @@ class ReplyController {
         const replay = await ReplyModel.create(ctx.request.body)
         ctx.body = res.success('回复成功')
     }
+    static async getReplyList(ctx,next) {
+        const comment_id = ctx.params.comment_id
+        const comment = await CommentModel.findById({_id: comment_id})
+        if(!comment) {
+            throw new global.errs.NotFound('找不到相关评论')
+        }
+        const replyList = await ReplyModel.find({comment_id}).sort({'_id': -1})
+        ctx.body = res.json(replyList)
+    }
 }
 module.exports = ReplyController
--- routes/reply.js
+++ routes/reply.js
@@ -7,4 +7,5 @@ const secretKey = global.config.security.secretKey
 router.prefix('/api/v1')
 // comment
 router.post('/reply',jwtAuth({secret: secretKey}), ReplyController.createReply)
+router.get('/reply/:comment_id',jwtAuth({secret: secretKey}), ReplyController.getReplyList)
 module.exports = router
